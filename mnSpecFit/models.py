ac7d761b6e9a571ff753df473ae183dbee6a04a6
J. Michael Burgess <jmichaelburgess@gmail.com>
2014-06-26 17:23:38 +0200

    added smoothly broken power law to model lookup

--- mnSpecFit/models.py
+++ mnSpecFit/models.py
@@ -1,5 +1,6 @@
 from Band import Band
 from BandBB import BandBB
 from PL2BB import PL2BB
+from SBPL import SBPL 
 from Synchrotron import Synchrotron
-models = {"Band":Band, "Band+BB": BandBB, "PL2BB": PL2BB,"Synchrotron":Synchrotron}
+models = {"Band":Band, "Band+BB": BandBB, "PL2BB": PL2BB,"Synchrotron":Synchrotron, "SBPL":SBPL}
