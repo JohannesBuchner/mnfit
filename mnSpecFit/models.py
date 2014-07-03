896ea661bba17ebff5bbca43548d649ffa94e592
J. Michael Burgess <jmichaelburgess@gmail.com>
2014-07-03 15:38:35 +0200

    added 2BB

--- mnSpecFit/models.py
+++ mnSpecFit/models.py
@@ -4,4 +4,11 @@ from PL2BB import PL2BB
 from SBPL import SBPL 
 from Synchrotron import Synchrotron
 from SynchrotronBB import SynchrotronBB
-models = {"Band":Band, "Band+BB": BandBB, "PL2BB": PL2BB,"Synchrotron":Synchrotron,"SynchrotronBB":SynchrotronBB ,"SBPL":SBPL}
+from BB2 import BB2
+models = {"Band":Band,\
+          "Band+BB": BandBB,\
+          "PL2BB": PL2BB,\
+          "Synchrotron":Synchrotron,\
+          "SynchrotronBB":SynchrotronBB ,\
+          "SBPL":SBPL,\
+          "Two BBs":BB2}
