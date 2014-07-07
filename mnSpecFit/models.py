379ef1c4640462c110f4bf24603ad0e8aa072266
J. Michael Burgess <jmichaelburgess@gmail.com>
2014-07-07 15:33:15 +0200

    added synch function

--- mnSpecFit/models.py
+++ mnSpecFit/models.py
@@ -2,13 +2,18 @@ from Band import Band
 from BandBB import BandBB
 from PL2BB import PL2BB
 from SBPL import SBPL 
+
 from Synchrotron import Synchrotron
+from SynchrotronComplex import SynchrotronComplex
 from SynchrotronBB import SynchrotronBB
+
 from BB2 import BB2
+
 models = {"Band":Band,\
           "Band+BB": BandBB,\
           "PL2BB": PL2BB,\
           "Synchrotron":Synchrotron,\
+          "SynchrotronComplex":SynchrotronComplex,\
           "SynchrotronBB":SynchrotronBB ,\
           "SBPL":SBPL,\
           "Two BBs":BB2}
