484db8e29bfa31b2292381b7e7c36a3c2d7d54a4
J. Michael Burgess <jmichaelburgess@gmail.com>
2014-07-08 18:22:30 +0200

    added glue for synchrotron power law

--- mnSpecFit/synchrotron_glue.pyx
+++ mnSpecFit/synchrotron_glue.pyx
@@ -2,8 +2,9 @@ cimport cython
 
 cdef extern from "synchrotron.h":
     double synchrotron(double,double,double,double)
-
-
+    double synchrotronComplex(double,double,double,double,double,double)
+    double synchrotronPL(double, double, double, double, double)
+{
 
 
 
@@ -11,4 +12,11 @@ cpdef double synchrotronPy(double energy, double norm, double estar, double inde
 
     return synchrotron(energy, norm, estar, index)
 
+cpdef double synchrotronComplexPy(double energy, double norm, double estar, double gammaMin, double gammaTH, double index):
+
+    return synchrotronComplex(energy, norm, estar, gammaMin, gammaTH, index)
+
+cpdef double synchrotronPLPy(double energy, double norm, double estar, double index, double gammaMin):
+
+    return synchrotronPL(energy, norm, estar, index, gammaMin)
 
