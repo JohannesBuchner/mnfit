2452dc3aa93dc4f9e997de22b1b43a846994ed3a
J. Michael Burgess <jmichaelburgess@gmail.com>
2014-07-10 13:44:31 +0200

    added fast cooling prototypes

--- mnSpecFit/synchrotron.h
+++ mnSpecFit/synchrotron.h
@@ -17,3 +17,38 @@ double electronDist(double gamma, double norm, double index, double gammaMin, do
   
   
 struct synch_params {double energy; double norm; double estar; double index;};
+
+/*         */
+
+
+double intergrandComplex(double gamma, void *p);
+
+
+double synchrotronComplex(double energy, double norm, double estar, double gammaMin, double gammaTH, double index);
+
+
+struct synch_params_complex {double energy; double norm; double estar; double gammaMin; double gammaTH; double index;};
+
+/*         */
+double synchrotronPL(double energy, double norm, double estar, double index, double gammaMin);
+
+
+
+double intergrandPL(double gamma, void *p);
+
+double electronPL(double gamma, double norm, double index, double gammaMin);
+
+
+struct synch_params_pl {double energy; double norm; double estar; double gammaMin; double index;};
+
+/*         */
+
+double synchrotronFast(double energy, double norm, double estar, double index);
+
+double electronDistFast(double gamma, double norm, double index, double gammaMin);
+
+double intergrandFast(double gamma, void *p);
+
+struct synch_params_pl {double energy; double norm; double estar; double index;  double gammaMin;};
+
+
