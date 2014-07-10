81f6113c0abb6e523742ab16f64cc0ab7137f159
J. Michael Burgess <jmichaelburgess@gmail.com>
2014-07-10 16:33:51 +0200

    added power law cutoff synch

--- mnSpecFit/synchrotron_glue.pyx
+++ mnSpecFit/synchrotron_glue.pyx
@@ -5,21 +5,34 @@ cdef extern from "synchrotron.h":
     double synchrotronComplex(double,double,double,double,double,double)
     double synchrotronPL(double, double, double, double, double)
     double synchrotronFast(double, double, double, double, double)
+    double synchrotronPL_cutoff(double, double, double, double, double, double)
 
-
-
+#Synchrotron from shock accelerated electrons
 cpdef double synchrotronPy(double energy, double norm, double estar, double index):
 
     return synchrotron(energy, norm, estar, index)
 
+
+
+#Synchrotron from shock accelerated electrons but with lots of parameters
 cpdef double synchrotronComplexPy(double energy, double norm, double estar, double gammaMin, double gammaTH, double index):
 
     return synchrotronComplex(energy, norm, estar, gammaMin, gammaTH, index)
 
+
+#Synchrotron from a power law of electrons
 cpdef double synchrotronPLPy(double energy, double norm, double estar, double index, double gammaMin):
 
     return synchrotronPL(energy, norm, estar, index, gammaMin)
 
+#Synchrotron from a power-law with a high-energy cutoff due to a max gamma
+cpdef double synchrotronPL_CO_Py(double energy, double norm, double estar, double index, double gammaMin, double gammaMax):
+
+    return synchrotronPL(energy, norm, estar, index, gammaMin, gammaMax)
+
+
+
+#Simple Fast cooled synchrotron
 cpdef double synchrotronFastPy(double energy, double norm, double estar, double index, double gammaMin):
 
     return synchrotronFast(energy, norm, estar, index, gammaMin)
