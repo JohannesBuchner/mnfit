0e26b24adaeab7f26a33ff167d43363b62f6cb28
J. Michael Burgess <jmichaelburgess@gmail.com>
2014-07-11 17:17:19 +0200

    fixed up

---
+++ mnSpecFit/models/TsviFast.py
@@ -0,0 +1,53 @@
+from mnfit.mnSpecFit.Model import Model
+#from mnfit.mnSpecFit.synchrotron_glue import synchrotronPy
+from numpy import power
+from mnfit.priorGen import *
+
+
+class TsviFast(Model):
+
+
+    def __init__(self):
+
+
+        def tsvinchrotron(ene,norm,vm,vc,index):
+
+            norm = power(10.,norm)
+
+            if ene <= vc:
+
+                val = power(ene/vc,-.6666)
+            elif vc < ene and ene <= vm:
+
+                val = power(ene/vc,-1.5)
+            else:
+
+                val = power(vm/vc,-1.5)*power(ene/vm,-(index+2.)*.5)
+            val=val*norm
+            return val
+            
+            
+
+
+        def TsvPrior(params, ndim, nparams):
+         
+
+            params[0] = jefferysPrior(params[0],1E-15,1.)
+            params[1] = uniformPrior(params[1], 10., 20000.)
+            params[2] = uniformPrior(params[2], 10.,20000)
+            params[3] = uniformPrior(params[3], 2., 5.)
+            
+             
+            pass
+
+       
+
+
+        self.modName = "TsviFast"
+        self.model=tsvinchrotron
+        self.prior=SynchPrior
+        self.n_params = 4
+        self.parameters = ["logNorm",r"$\v_m$", r"$\v_c$",r"$\delta$"]
+
+
+
