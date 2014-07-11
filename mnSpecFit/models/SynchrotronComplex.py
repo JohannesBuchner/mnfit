230dd0a8a07c6c76a77756b4d29e2d9878febc26
J. Michael Burgess <jmichaelburgess@gmail.com>
2014-07-11 19:52:57 +0200

    fixed another bug

--- mnSpecFit/models/SynchrotronComplex.py
+++ mnSpecFit/models/SynchrotronComplex.py
@@ -22,7 +22,7 @@ class SynchrotronComplex(Model):
             params[0] = jefferysPrior(params[0],1E-15,1.)
             params[1] = uniformPrior(params[1], 0., 3.)
             params[2] = uniformPrior(params[2], 1., 1800.)
-            params[3] = uniformPrior(params[3],1.900.)
+            params[3] = uniformPrior(params[3],1,900.)
             params[4] = uniformPrior(params[4], 2., 12.)#Must be positive!
              
             pass
