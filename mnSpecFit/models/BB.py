4323adf1d6d7876001dd2c4252c7087ff8f7e594
J. Michael Burgess <jmichaelburgess@gmail.com>
2014-08-21 05:50:06 +0200

    added this model

---
+++ mnSpecFit/models/BB.py
@@ -0,0 +1,56 @@
+from mnfit.mnSpecFit.Model import Model
+from numpy import exp, power, zeros
+from mnfit.priorGen import *
+
+
+
+
+
+
+
+
+class BB(Model):
+
+   def __init__(self):
+
+
+
+
+      def bb(x,logA,kT):
+         
+         val = power(10.,logA)*power(x,2.)*power( exp(x/float(kT)) -1., -1.)
+         return val
+
+
+
+      self.paramsRanges = [[1.E-6,1.E3,"J"],[1.E2,1.E5,"U"],[-2.,1.,"U"],[-10.,-2,"U"]]
+     
+
+
+
+      def BBPrior(params, ndim, nparams):
+
+         for i in range(ndim):
+            params[i] = priorLU[self.paramsRanges[i][-1]](params[i],self.paramsRanges[i][0],self.paramsRanges[i][1])
+     
+     
+      bbDict = {"params":\
+                [r"logN$_{\rm BB}$","kT"],\
+                "model":bb\
+      }
+
+      self.componentLU={"Band":bandDict,\
+                        "Blackbody":bbDict\
+      }
+
+      
+      self.modName = "Band+BB"
+      self.model=bb
+      self.prior=BBPrior
+      self.n_params = 2
+      self.parameters = [r"logN$_{\rm BB}$","kT"]
+
+      self._modelDict = {"params":self.parameters,\
+                         "model":bb\
+      }
+      self._composite = False
