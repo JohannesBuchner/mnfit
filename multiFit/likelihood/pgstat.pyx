2870a44f21c45af716fe0ffc803f73d323f1fc7e
J. Michael Burgess <jmichaelburgess@gmail.com>
2015-05-24 14:04:50 +0200

    about to try something cool

--- multiFit/likelihood/pgstat.pyx
+++ multiFit/likelihood/pgstat.pyx
@@ -13,7 +13,10 @@ cdef extern from "math.h":
     double sqrt(double)
 
 
-    
+DTYPE = np.double
+ctypedef np.double_t DTYPE_t
+
+        
 class pgstat(Likelihood):
 
 
@@ -46,60 +49,22 @@ class pgstat(Likelihood):
 
         self.modc = mc
 
-    def ComputeLikelihood_SLOW(self):
-
-        cdef double tr = self.ts/self.tb
-        cdef double FLOOR = 1.0e-5
-        cdef double fi = 0.
-        cdef double a,b,c,sign,q, yb
-        cdef double stat = 0.
-
-        for i in range(len(self.counts)):
-
-            if self.berr[i] == 0:
-                yb = max(self.modc[i]+self.bg[i],FLOOR/self.ts)
-                stat+= self.ts*yb
-                if self.counts[i] > 0.:
-                    stat += self.counts[i]*(log(self.counts[i])-log(self.ts*yb)-1)
-                
-            ###################
-            if self.counts[i] == 0:
-                stat+=self.ts*self.modc[i] + self.bg[i]*tr - 0.5*self.berr[i]*tr*tr
-
-            else:
-                # Solve quadratic equation for fi, using Numerical Recipes technique
-                # to avoid round-off error that can easily cause problems here
-                # when b^2 >> ac.
-                #
-               
-                a = self.tb*self.tb
-                b = self.ts*self.berr[i] - self.tb*self.tb + self.tb*self.tb*self.modc[i]
-                c = self.ts*self.berr[i]*self.modc[i] - self.counts[i]*self.berr[i] - self.tb*self.bg[i]*self.modc[i]
-                    
-                if b>=0:
-                    sign=1.
-                else:
-                    sign = -1.
-                q = -.5*(b + sign*sqrt(b*b - 4.0*a*c))
-                fi = q/a
-                if fi< 0.:
-                    fi = c/q
-                stat+= self.ts*(self.modc[i] + fi)
-                stat-= self.counts[i]*log(self.ts*self.modc[i]+self.ts*fi)
-                stat+= 0.5*(self.bg[i]-self.tb*fi)*(self.bg[i]-self.tb*fi)/self.berr[i]
-                stat-= self.counts[i]*(1-log(self.counts[i]))
-                
-
-        return stat
             
-
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    @cython.cdivision(True)
     def ComputeLikelihood(self):
 
         cdef double tr = self.ts/self.tb
         cdef double FLOOR = 1.0e-5
-        #cdef double fi = 0.
-        #cdef double a,b,c,sign,q, yb
-        #cdef double stat = 0.
+        cdef np.ndarray[np.double_t,ndim=1] fi
+        cdef double a
+        cdef np.ndarray[np.double_t,ndim=1] b,c,sign,q,yb
+
+        cdef double tb = self.tb
+        cdef double ts = self.ts
+
+        cdef np.ndarray[np.double_t,ndim=1] stat, modc, bg, counts, berr
 
 
 
@@ -107,7 +72,7 @@ class pgstat(Likelihood):
         fi = np.zeros(len(self.counts))
 
 
-        a = self.tb*self.tb
+        a = tb*tb
         b = self.ts*self.berr - self.tb*self.bg + self.tb*self.tb*self.modc
         c = self.ts*self.berr*self.modc - self.counts*self.berr - self.tb*self.bg*self.modc
         sign = np.sign(b)
