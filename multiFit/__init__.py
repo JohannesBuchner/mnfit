4c9b66893b381d4b76e2fc408fb0be917a06d0e6
J. Michael Burgess <jmichaelburgess@gmail.com>
2015-05-28 15:32:31 +0200

    fixed init

--- multiFit/__init__.py
+++ multiFit/__init__.py
@@ -2,4 +2,4 @@
 from likelihood.pgstat import pgstat
 from likelihood.chi2 import chi2
 from likelihood.cstat import cstat
-from likelihood.cstat_rmfit import cstat_rmfit
+from likelihood.rmfit import rmfit
